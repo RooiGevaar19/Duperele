ed84ed7207a374ce3d05094327fdfe14608f6196
RooiGevaar19 <redthreat19@gmail.com>
2020-06-05 17:05:50 +0200

    zadanko w R

---
+++ R/sp6/zadanie.r
@@ -0,0 +1,31 @@
+# Zadanie 1
+
+n = 11
+Mu = 42436.97 
+Si = 2189.87
+lim = 45000
+#alt = "less"
+alpha = 0.03
+
+val1 <- ((Mu-lim)*sqrt(n-1))/Si
+val2 <- pt(alpha, n-1) # dla hipotezy alternatywnej <=
+val1
+val2
+val1 <= -val2
+
+# t.test(x = wm, y = ws, alternative = c("two.sided", "less", "greater"), var.equal = TRUE)
+#t.test(x = wm, y = ws, alternative = c("less"), var.equal = TRUE, conf.level = 1-alfa_3)
+
+# Zadanie 2
+
+n = 20
+Si = 0.84
+#alt = "greater"
+alpha = 0.07
+lim = 0.45
+
+val1 <- (n*Si)/lim
+val2 <- pchisq(alpha, n-1)
+val1
+val2
+val1 >= val2
