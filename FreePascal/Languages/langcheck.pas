30321511748a259df1456112c20cd3c18da1675b
RooiGevaar19 <redthreat19@gmail.com>
2021-03-23 23:09:17 +0100

    detect languages in fpc

---
+++ FreePascal/Languages/langcheck.pas
@@ -0,0 +1,32 @@
+program langcheck;
+
+{$mode objfpc}
+
+uses SysUtils;
+
+{$IFDEF MSWINDOWS}
+function GetLocaleInformation(Flag: integer): string;
+var
+  pcLCA: array[0..20] of char;
+begin
+  if (GetLocaleInfo(LOCALE_SYSTEM_DEFAULT, Flag, pcLCA, 19) <= 0) then
+  begin
+    pcLCA[0] := #0;
+  end;
+  Result := pcLCA;
+end;
+
+{$ENDIF}
+
+function GetLocaleLanguage: string;
+begin
+  {$IFDEF MSWINDOWS}
+   Result := GetLocaleInformation(LOCALE_SENGLANGUAGE);
+  {$ELSE}
+   Result := SysUtils.GetEnvironmentVariable('LANG');
+  {$ENDIF}
+end;   
+
+begin
+    writeln(GetLocaleLanguage);
+end.
